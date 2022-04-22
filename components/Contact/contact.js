ff834f8298bf5b995c6328ecc0379275a6c51955
mengiefen <mengiefen@gmail.com>
2022-04-22 22:42:31 +0300

    add contact form

--- components/Contact/contact.js
+++ components/Contact/contact.js
@@ -1,7 +1,59 @@
-import Container from './contact.styled';
+import Container, { Button } from './contact.styled';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  return <Container id="contact">Contact</Container>;
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const PUBLIC_KEY = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+    const TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID;
+    const SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_ID;
+     
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+      (result) => {
+        console.log(result.text);       
+      },
+      (error) => {
+        console.log(error.text);
+      },
+    );
+  };
+
+  return (
+    <Container id="contact">
+      <div className="welcome">
+        <h2>Contact me</h2>
+        <p>
+          I&apos;m always interested in hearing about new projects, so if
+          you&apos;d like to chat please get in touch.
+        </p>
+      </div>
+      <form ref={form} onSubmit={handleSubmit}>
+        <ul>
+          <li>
+            <input type="text" name="name" placeholder="Your name" />
+          </li>
+          <li>
+            <input type="email" name="email" placeholder="Valid email" />
+          </li>
+
+          <li>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Your message here..."
+              rows={5}
+            />
+          </li>
+        </ul>
+        <Button className="form-btn" type="submit">
+          Send message
+        </Button>
+      </form>
+    </Container>
+  );
 };
 
 export default Contact;
--- components/Contact/contact.styled.js
+++ components/Contact/contact.styled.js
@@ -1,13 +1,97 @@
 import styled from 'styled-components';
 
 const Container = styled.section`
-  width: 100%;
+  width: 90%;
   height: 100vh;
   background-color: ${(props) => props.theme.bg};
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
   color: ${(props) => props.theme.primary};
+  padding: 1rem 5%;
+
+  .welcome {
+    display: flex;
+    flex-direction: column;
+
+    h2 {
+      text-transform: uppercase;
+      font-size: 1.75rem;
+      font-weight: 700;
+    }
+    p {
+      font-weight: 400;
+      line-height: 1.5;
+      margin-bottom: 1rem;
+    }
+  }
+  form {
+    width: 60%;
+    text-align: right;
+  }
+
+  ul {
+    list-style: none;
+    margin-bottom: 2rem;
+  }
+
+  li {
+    margin-bottom: 1rem;
+  }
+
+  input {
+    padding: 1rem 0.5rem;
+    font-size: 1rem;
+    outline: 2px solid transparent;
+    border: none;
+    width: 100%;
+    background-color: ${({ theme }) => theme.bg};
+    color: ${({ theme }) => theme.primary};
+    border: 0.5px solid ${({ theme }) => theme.primary};
+    transition: all 0.2s ease-in;
+
+    &:hover,
+    &:focus {
+      border: 0.5px solid transparent;
+      outline: 2px solid ${({ theme }) => theme.secondary};
+    }
+  }
+
+  textarea {
+    padding: 0.5rem;
+    font-size: large;
+    outline: 2px solid transparent;
+    border: none;
+    width: 100%;
+    background-color: ${({ theme }) => theme.bg};
+    color: ${({ theme }) => theme.primary};
+    border: 0.5px solid ${({ theme }) => theme.primary};
+    transition: all 0.2s ease-in;
+
+    &:hover,
+    &:focus {
+      border: 0.5px solid transparent;
+      outline: 2px solid ${({ theme }) => theme.secondary};
+    }
+  }
+`;
+
+export const Button = styled.button`
+  background-color: ${(props) => props.theme.secondary};
+  border: 2px solid ${(props) => props.theme.secondary};
+  color: ${(props) => props.theme.primary};
+  min-width: 150px;
+  text-transform: uppercase;
+  font-size: 1rem;
+  font-weight: 700;
+  letter-spacing: 1.5px;
+  padding: 1rem;
+  cursor: pointer;
+  transition: transform 0.3s ease-out;
+
+  &:hover {
+    transform: scale(0.98);
+  }
 `;
 
 export default Container;
