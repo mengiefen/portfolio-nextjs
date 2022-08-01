402a52bafdec8854e8e7c0bcd0e753cc8a99e6f1
mengiefen <mengiefen@gmail.com>
2022-08-01 20:37:14 +0300

    fix linter error

--- components/Skills/ProfessionalSkills.js
+++ components/Skills/ProfessionalSkills.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Image from "next/image";
-import Container from "./skills.styled";
-import { professionalSkill } from "../../data/skillLogo";
-import Skills from "./skills";
+import React from 'react';
+import Image from 'next/image';
+import Container from './skills.styled';
+import { professionalSkill } from '../../data/skillLogo';
+import Skills from './skills';
 
 const ProfessionalSkills = () => {
   return (
     <div className="prof-skill-set">
       {professionalSkill.map((skill) => (
-        <div className="prof-skills">
-          <div key={skill.id} className="skill-icon-holder">
+        <div className="prof-skills" key={skill.id}>
+          <div className="skill-icon-holder">
             <Image
               src={skill.img}
               width={50}
               height={50}
               alt={skill.alt}
-              style={{ color: "red" }}
+              style={{ color: 'red' }}
             ></Image>
           </div>
           <p className="skill-desc">{skill.desc}</p>
