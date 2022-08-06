18de758f9d4ae187d425def2f3ee59f7b1b8942d
mengiefen <mengiefen@gmail.com>
2022-08-06 03:39:51 -0700

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   render() {
@@ -12,6 +12,24 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Arbutus&family=Bebas+Neue&family=Edu+TAS+Beginner:wght@400;500;600;700&family=Margarine&family=Nova+Oval&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&family=Pacifico&family=Yatra+One&family=Zen+Dots&display=swap"
             rel="stylesheet"
           />
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
@@ -20,6 +38,7 @@ export default class MyDocument extends Document {
       </Html>
     );
   }
+
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
