7e5466689233b54bc9728245c13523b2aba88822
mengiefen <mengiefen@gmail.com>
2022-10-27 22:19:32 +0300

    chnage toast message

--- components/FrontPage/front-page.js
+++ components/FrontPage/front-page.js
@@ -10,70 +10,68 @@ import particlesConfig from '../../data/particlesConfig.json';
 import FlashMessage from '../FlashMessages/FlashMessage';
 const FrontPage = () => {
   const message =
-    "Thanks for visiting my portfolio. I'm a passionate developer looking for new opportunities to serve, grow and learn new technologies.";
+    "Thanks for visiting my portfolio. I'm a passionate developer looking for new opportunities. Feel free to contact me.";
   const particlesInit = async (main) => {
     await loadFull(main);
   };
 
   return (
-   
-      <Container id="home">
-        <FlashMessage message={message} duration={100000} />
+    <Container id="home">
+      <FlashMessage message={message} duration={100000} />
 
-        <div style={{ zIndex: '-1' }}>
-          <Particles
-            id="tsparticles"
-            init={particlesInit}
-            height="100vh"
-            width="100vw"
-            options={particlesConfig}
+      <div style={{ zIndex: '-1' }}>
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          height="100vh"
+          width="100vw"
+          options={particlesConfig}
+        />
+      </div>
+
+      <SocialMedia />
+      <aside className="text-area">
+        <h1 className="heading">
+          <span className="greeting">
+            Hi <span className="wave">👋</span>
+          </span>
+
+          <span className="my-name"> I&apos;m Mengstu</span>
+        </h1>
+        <h2 className="sub-heading">
+          <Typed
+            strings={['Frontend', 'Backend', 'Fullstack']}
+            typeSpeed={80}
+            backSpeed={80}
+            backDelay={1000}
+            loop
           />
-        </div>
-
-        <SocialMedia />
-        <aside className="text-area">
-          <h1 className="heading">
-            <span className="greeting">
-              Hi <span className="wave">👋</span>
-            </span>
-
-            <span className="my-name"> I&apos;m Mengstu</span>
-          </h1>
-          <h2 className="sub-heading">
-            <Typed
-              strings={['Frontend', 'Backend', 'Fullstack']}
-              typeSpeed={80}
-              backSpeed={80}
-              backDelay={1000}
-              loop
-            />
-            Developer
-          </h2>
-          <p className="paragraph">
-            I can help you build a product, feature, or website. Look through
-            some of my work and experience! If you like what you see and have a
-            project you need to be coded, don’t hesitate to contact me.
-          </p>
-          <ButtonOutline
-            href="#contact"            
-            whileInView={{  scale: [0.6, 1, 0.75, 1] }}
-            transition={{ times: [0, 0.1, 0.9, 1] }}
-          >
-            Contact me
-          </ButtonOutline>
-        </aside>
-        <figure>
-          <span className="overlay"></span>
-          <Image
-            src={bgImg}
-            width={1900}
-            height={1800}
-            alt="picture of me"
-            className="img"
-          />
-        </figure>
-      </Container>
-    
+          Developer
+        </h2>
+        <p className="paragraph">
+          I can help you build a product, feature, or website. Look through some
+          of my work and experience! If you like what you see and have a project
+          you need to be coded, don’t hesitate to contact me.
+        </p>
+        <ButtonOutline
+          href="#contact"
+          whileInView={{ scale: [0.6, 1, 0.75, 1] }}
+          transition={{ times: [0, 0.1, 0.9, 1] }}
+        >
+          Contact me
+        </ButtonOutline>
+      </aside>
+      <figure>
+        <span className="overlay"></span>
+        <Image
+          src={bgImg}
+          width={1900}
+          height={1800}
+          alt="picture of me"
+          className="img"
+        />
+      </figure>
+    </Container>
   );
 };
 
