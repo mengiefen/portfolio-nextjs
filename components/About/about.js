2adbc84c04f30432ec37d505cd5486a9e239c94a
Mengstu <mengiefen@gmail.com>
2023-04-21 07:27:31 +0000

    upgrade to next.js 13

--- components/About/about.js
+++ components/About/about.js
@@ -22,13 +22,7 @@ const About = () => {
           whileInView={{ scale: 1 }}
           transition={{ ease: "easeOut", duration: 1 }}
         >
-          <Image
-            src={bgImg}
-            width={1900}
-            height={1800}
-            alt="picture of me"
-            className="img"
-          />
+          <Image src={bgImg} width={1900} height={1800} alt="picture of me" className="img" />
         </motion.figure>
         <motion.aside
           className="desc"
@@ -38,82 +32,17 @@ const About = () => {
         >
           <h2 className="title">About Me</h2>
           <p className="general-desc">
-            I am a Full-stack Developer who enjoys learning new programming
-            languages and frameworks. I am the kind of person who likes
-            challenging myself and learning from it. I can do full-featured
-            front-end websites using HTML, CSS, JavaScript, and React. I&apos;m
-            also an electrical engineer who loves working on embedded and power
-            electronics & drives.
+            I am a Full-stack Developer who enjoys learning new programming languages and frameworks. I am the
+            kind of person who likes challenging myself and learning from it. I can do full-featured front-end
+            websites using HTML, CSS, JavaScript, and React. I&apos;m also an electrical engineer who loves
+            working on embedded and power electronics & drives.
           </p>
 
           <p className="general-desc">
-            I worked also as an engineering instructor in local college for more
-            than 11 years This experiance coupled with my skill in programming
-            gave me an opportunity to adapt differnet working enviroment
+            I worked also as an engineering instructor in local college for more than 11 years This experiance
+            coupled with my skill in programming gave me an opportunity to adapt differnet working enviroment
             responsibly.
           </p>
-          {/* <div className="card-list">
-              <div className="card">
-                <div className="card-detail">
-                  <Image
-                    src={communication}
-                    width={50}
-                    height={50}
-                    alt="communication icons"
-                    className="card-icon"
-                  />
-                  <h4 className="card-title">GOOD COMMUNICATION</h4>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-detail">
-                  <Image
-                    src={webdesign}
-                    width={50}
-                    height={50}
-                    alt="communication icons"
-                    className="card-icon"
-                  />
-                  <h4 className="card-title">EXCELLENT PROGRAMMING SKILLS</h4>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-detail">
-                  <Image
-                    src={test}
-                    width={50}
-                    height={50}
-                    alt="communication icons"
-                    className="card-icon"
-                  />
-                  <h4 className="card-title">TEST DRIVEN DEVELOPEMNT</h4>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-detail">
-                  <Image
-                    src={workflows}
-                    width={50}
-                    height={50}
-                    alt="communication icons"
-                    className="card-icon"
-                  />
-                  <h4 className="card-title">WORK FLOWS</h4>
-                </div>
-              </div>
-              <div className="card">
-                <div className="card-detail">
-                  <Image
-                    src={remotework}
-                    width={50}
-                    height={50}
-                    alt="communication icons"
-                    className="card-icon"
-                  />
-                  <h4 className="card-title">REMOTE PAIR PROGRAMMING</h4>
-                </div>
-              </div>
-            </div> */}
 
           <Button
             type="underline"
